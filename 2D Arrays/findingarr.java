e6e2db453567df5eebf71ae40f77cd7f931085f5
Jeet Biswas <125785193+Jeetbiswas23@users.noreply.github.com>
2023-10-26 23:21:28 +0530

    Add files via upload

---
+++ 2D Arrays/findingarr.java
@@ -0,0 +1,42 @@
+package TwodArray;
+
+import java.security.Key;
+import java.util.*;
+
+public class findingarr {
+    public static boolean search(int matrix[][], int key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("found at cell (" + i + "," + j + ")");
+                    return true;
+                }
+
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+
+    }
+    public static void main(String args[]) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length; //n is row and m is a column
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        //Output
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int key=3;
+        search(matrix, key);
+
+    }
+}
