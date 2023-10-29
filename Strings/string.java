ea4a3ec58bb73830057116e86a543800ebe9b308
Jeet Biswas <125785193+Jeetbiswas23@users.noreply.github.com>
2023-10-29 11:10:20 +0530

    Add files via upload

---
+++ Strings/string.java
@@ -0,0 +1,25 @@
+package Strings;
+import java.util.*;
+
+public class string {
+    public static void main(String args[]) {
+        //For writting Strings
+        String str = "abcd";
+        String str1 = new String("xyz");
+
+        //Strings are IMMUTABLE(It can't be changed)
+
+        //Input and Output of Strings
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println(name);
+
+        // //String Length
+        // String Fullname = "Tony Stark";
+        // System.out.println(Fullname.length());
+
+
+    }
+    
+}
