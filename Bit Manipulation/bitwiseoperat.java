6073b64d22f81eb6b8f19eb5fcde62c0cdaff268
Jeet Biswas <125785193+Jeetbiswas23@users.noreply.github.com>
2023-11-03 18:20:38 +0530

    Add files via upload

---
+++ Bit Manipulation/bitwiseoperat.java
@@ -0,0 +1,14 @@
+package BitManipulation;
+
+public class bitwiseoperat {
+    public static void main(String args[]) {
+        System.out.println((5 & 6));//Binary AND
+        System.out.println((5 | 6));//Binary OR
+        System.out.println((5 ^ 6));//Binary XOR
+        System.out.println((~5));//Binary One's Complement
+        System.out.println((5 << 2));//Binary Left Shift
+        System.out.println((6 >> 1));//Binary Right Shift 
+        
+    }
+    
+}
