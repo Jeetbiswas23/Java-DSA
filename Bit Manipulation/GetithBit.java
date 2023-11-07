5fc31a5c13154ed32e210f9cebb82162b699b755
Jeet Biswas <125785193+Jeetbiswas23@users.noreply.github.com>
2023-11-07 10:20:09 +0530

    Update GetithBit.java

--- Bit Manipulation/GetithBit.java
+++ Bit Manipulation/GetithBit.java
@@ -16,3 +16,20 @@ public class GetithBit {
     }
     
 }
+/*int n = 10; // Binary representation: 1010
+int i = 3;  // We want to get the value of the 3rd bit (from the right)
+
+// 1 << i = 1 << 3 = 8
+// Binary representation of 8: 1000
+
+// n & Bitmask:
+//  1010
+//& 1000
+//-----------
+//  1000
+//-----------
+
+// Since the result is 1000 in binary, which is equivalent to 8 in decimal, the function will return 1 because the 3rd bit is 1.
+
+System.out.println(getithBit(10, 3)); // Output: 1
+ */
