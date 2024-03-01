c56040093742b231136d9bebee9f780dd4cbf8de
Jeet Biswas <125785193+Jeetbiswas23@users.noreply.github.com>
2024-03-01 16:41:01 +0530

    Add files via upload

---
+++ ArrayList/introarrlist.java
@@ -0,0 +1,11 @@
+package Arraylist;
+import java.util.ArrayList;
+
+public class introarrlist {
+    public static void main(String args[]) {
+        ArrayList<Integer> List = new ArrayList<>();
+        ArrayList<String> Lis1t = new ArrayList<>();
+        ArrayList<Boolean> Lis2t = new ArrayList<>();
+    }
+    
+}
