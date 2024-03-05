2a0a46b0621424d72229d373f28ffbdaf3e568b8
Jeet Biswas <125785193+Jeetbiswas23@users.noreply.github.com>
2024-03-05 10:33:22 +0530

    Add files via upload

---
+++ ArrayList/reverseal.java
@@ -0,0 +1,17 @@
+package Arraylist;
+import java.util.*;
+
+public class reverseal {
+    public static void main(String args[]) {
+        ArrayList<Integer> List = new ArrayList<>();
+        List.add(1);
+        List.add(2);
+        List.add(3);
+        List.add(4);
+        for (int i = List.size() - 1; i >= 0; i--) {
+            System.out.print(List.get(i)+" ");
+        }
+        System.out.println();;  
+    }
+    
+}
